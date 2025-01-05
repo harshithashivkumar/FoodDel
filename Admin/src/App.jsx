6ad2f8c024c3cf0aabe649669706dcf017ca317c
Dinesh L <harshithashivkumar3112@gmail.com>
2025-01-05 20:23:45 +0530

    commit

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -9,7 +9,7 @@ import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
-  const url='https://fooddelbackend-j53f.onrender.com'
+  const url='https://fooddel-f1cz.onrender.com'
   return (
     <div className="app">
       <ToastContainer/>
